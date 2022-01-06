9cc0ca1297025d3bf168b03db9c12297c195ebce
Roman Moskalenko <skalermo@gmail.com>
2022-01-06 22:02:25 +0100

    Add new protocol entry to metadata

--- agents/informationBroker.py
+++ agents/informationBroker.py
@@ -6,14 +6,22 @@ from messages import requestManagement
 
 
 class InformationBrokerAgent(Agent):
-    class RecvBehav(OneShotBehaviour):
+    async def setup(self):
+        self.requests = [{'id': 1, 'category': 'salt', 'comment': 'Himalaya salt', 'username': 'user1@localhost'}]
+        print("ReceiverAgent started")
+        b = self.UserRequestsBehav()
+        template = Template()
+        template.set_metadata("performative", "request")
+        template.set_metadata("protocol", "user_requests")
+        self.add_behaviour(b, template)
+
+    class UserRequestsBehav(OneShotBehaviour):
         async def run(self):
             print("RecvBehav running")
-            self.requests = [{'id': 1, 'category': 'salt', 'comment': 'Himalaya salt', 'username': 'user1@localhost'}]
             msg = await self.receive(timeout=1000)  # wait for a message for 10 seconds
             if msg:
                 if msg.metadata['performative'] == 'request':
-                    resp = requestManagement.Response(to=str(msg.sender), data=self.requests)
+                    resp = requestManagement.Response(to=str(msg.sender), data=self.agent.requests)
                     await self.send(resp)
                 print("Message received with content: {}".format(msg.body))
             else:
@@ -21,10 +29,3 @@ class InformationBrokerAgent(Agent):
 
             # stop agent from behaviour
             await self.agent.stop()
-
-    async def setup(self):
-        print("ReceiverAgent started")
-        b = self.RecvBehav()
-        template = Template()
-        template.set_metadata("performative", "request")
-        self.add_behaviour(b, template)
--- messages/requestManagement.py
+++ messages/requestManagement.py
@@ -8,9 +8,9 @@ from config import ONTOLOGY
 class Response(Message):
     def __init__(self, to, data):
         body = json.dumps(data)
-        super().__init__(to=to, body=body, metadata={'performative': 'inform', 'ontology': ONTOLOGY})
+        super().__init__(to=to, body=body, metadata={'performative': 'inform', 'protocol': 'user_requests', 'ontology': ONTOLOGY})
 
 
 class Retrieve(Message):
     def __init__(self, to):
-        super().__init__(to=to, metadata={'performative': 'request', 'ontology': ONTOLOGY})
+        super().__init__(to=to, metadata={'performative': 'request', 'protocol': 'user_requests', 'ontology': ONTOLOGY})
