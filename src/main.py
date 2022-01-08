b37f3f7994f0dc3a7b6bb6571b2271c89867841d
Roman Moskalenko <skalermo@gmail.com>
2022-01-08 11:46:51 +0100

    Clean up main.py

--- src/main.py
+++ src/main.py
@@ -1,28 +1,40 @@
 import time
 
+from spade.agent import Agent
 from agents.informationBroker import InformationBrokerAgent
 from agents.user import UserAgent
 from agents.brokerDirectory import BrokerDirectoryAgent
 
 
-def create_agent(agent_cls, jid):
-    return agent_cls(jid, "aasd")
+agents: list[Agent] = []
 
 
-if __name__ == "__main__":
-    bd_agent = create_agent(BrokerDirectoryAgent, "broker-directory@localhost")
-    _ = bd_agent.start()
-    rragent = create_agent(InformationBrokerAgent, "information-broker-1@localhost")
-    future = rragent.start()
-    future.result()  # wait for receiver agent to be prepared.
-    useragent = UserAgent("user1@localhost", "aasd")
-    useragent.start()
+def create_agent(agent_cls: Agent.__class__, jid: str):
+    agent: Agent = agent_cls(f'{jid}@localhost', "aasd")
+    agents.append(agent)
+    return agent
 
-    while rragent.is_alive():
+
+def main():
+    ib_agent = create_agent(InformationBrokerAgent, "information-broker-1")
+    create_agent(BrokerDirectoryAgent, "broker-directory")
+    create_agent(UserAgent, "user1")
+
+    for a in agents:
+        if isinstance(a, InformationBrokerAgent):
+            future = a.start()
+            future.result()
+        else:
+            a.start()
+
+    while ib_agent.is_alive():
         try:
             time.sleep(1)
         except KeyboardInterrupt:
-            useragent.stop()
-            rragent.stop()
-            break
+            for a in agents:
+                a.stop()
     print("Agents finished")
+
+
+if __name__ == "__main__":
+    main()
