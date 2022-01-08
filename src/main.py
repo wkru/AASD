20f67926c2112628f292861138c4d4ed612040ee
Roman Moskalenko <skalermo@gmail.com>
2022-01-08 12:57:14 +0100

    Create reviewCollector via factory

--- src/main.py
+++ src/main.py
@@ -1,18 +1,27 @@
 import time
 import logging
+from typing import Type
 
 from spade.agent import Agent
 from agents.informationBroker import InformationBrokerAgent
 from agents.user import UserAgent
 from agents.brokerDirectory import BrokerDirectoryAgent
-from agents.reviewCollector import ReviewCollectorAgent
+
+from factories.abstractFactory import AbstractFactory
+from factories.reviewCollector import ReviewCollectorFactory
 
 
 agents: list[Agent] = []
 
 
-def create_agent(agent_cls: Agent.__class__, jid: str):
-    agent: Agent = agent_cls(f'{jid}@localhost', "aasd")
+def create_agent(agent_cls: Type[Agent], jid: str) -> Agent:
+    agent = agent_cls(f'{jid}@localhost', "aasd")
+    agents.append(agent)
+    return agent
+
+
+def create_via_factory(factory_cls: Type[AbstractFactory]) -> Agent:
+    agent = factory_cls.create_agent()
     agents.append(agent)
     return agent
 
@@ -20,7 +29,7 @@ def create_agent(agent_cls: Agent.__class__, jid: str):
 def main():
     ib_agent = create_agent(InformationBrokerAgent, "information-broker-1")
     create_agent(BrokerDirectoryAgent, "broker-directory")
-    create_agent(ReviewCollectorAgent, 'review-collector-1')
+    create_via_factory(ReviewCollectorFactory)
     create_agent(UserAgent, "user1")
 
     for a in agents:
