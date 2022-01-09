91e2a29f77b2966b60465375fa460bea7c46d887
Roman Moskalenko <skalermo@gmail.com>
2022-01-09 12:47:51 +0100

    Move create_agent() to utils.py

--- test/utils.py
+++ test/utils.py
@@ -7,3 +7,7 @@ def wait_and_get(agent: Agent, key: str):
     while (obj := agent.get(key)) is None:
         sleep(0.001)
     return obj
+
+
+def create_agent(agent_cls, jid):
+    return agent_cls(f'{jid}@localhost', 'aasd')
