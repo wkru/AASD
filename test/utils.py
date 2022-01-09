c9dc4cd6fc8a37c6e1d4137f4c67cbd9b913003d
Roman Moskalenko <skalermo@gmail.com>
2022-01-09 12:48:24 +0100

    Make wait_and_get() check against passed argument

--- test/utils.py
+++ test/utils.py
@@ -3,8 +3,11 @@ from time import sleep
 from spade.agent import Agent
 
 
-def wait_and_get(agent: Agent, key: str):
-    while (obj := agent.get(key)) is None:
+def wait_and_get(agent: Agent, key: str, value_to_check=None):
+    while True:
+        obj = agent.get(key)
+        if obj is not None and (value_to_check is None or obj != value_to_check):
+            break
         sleep(0.001)
     return obj
 
